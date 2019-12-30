3a4ee38975220f9f373fc0dbf359edb2b865f2fc
Lalitweb <lalityadav.mail@gmail.com>
2019-12-30 16:11:46 +0530

    random-commit

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './Person.css';
-import Aux from './../../../hoc/auxiliary'
+import Aux from './../../../hoc/auxiliary';
+import withClass from './../../../hoc/withClass';
 
 
 //****************   Implementation of Person component using classes   *************/
@@ -14,14 +16,29 @@ class Person extends Component {
             {/* <div className={classes.Person}> */}
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input 
+                type="text" 
+                ref = {(inputElement) => {
+                    console.log("inputElement = ", inputElement);
+                    this.inputElement = inputElement;
+                    inputElement.focus();
+                }} 
+                onChange={this.props.changed} 
+                value={this.props.name} />
             {/* </div> */}
             </Aux>
         )
     }
 };
 
-export default Person;
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+}
+
+export default withClass(Person, classes.Person);
 
 
 
